872039cd49bb69501df655b465db0b8cc0943e5e
Edemone <edem.agbenyo@gmail.com>
2019-11-22 21:37:42 +0000

    Add contact page and link

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
 end
