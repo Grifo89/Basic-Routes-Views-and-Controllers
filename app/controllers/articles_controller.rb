806c329f172a52373e89c06e03aad61387655ec1
Edemone <edem.agbenyo@gmail.com>
2019-11-25 21:46:42 +0000

    Add action to access single article and list of articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,20 @@ class ArticlesController < ApplicationController
   def new
   end
 
-  def create
-    render plain: params[:article].inspect
+  def index
+    @articles = Article.all
   end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
