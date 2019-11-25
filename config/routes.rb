6b0551a3eb2e96241692f81f8ce8fa9045d0985e
Edemone <edem.agbenyo@gmail.com>
2019-11-25 20:23:02 +0000

    Add Article controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
