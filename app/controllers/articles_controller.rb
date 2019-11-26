5fa51a52e786ecbadd3cf4c914e03625bebcbc6f
Edemone <edem.agbenyo@gmail.com>
2019-11-26 17:43:19 +0000

    Add Edit and Delete features to CRUD articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def index
@@ -7,12 +8,35 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  
+    redirect_to articles_path
   end
   private
     def article_params
